a56a2556663b1df90a9cc7b0b208c6a4990d6171
Matias Salinas <salinasmatiasit@gmail.com>
2024-09-29 20:12:23 -0300

    NavBar component created

---
+++ web/src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+function NavBar() {
+    return (
+        <nav> 
+          <div className="logo">LOGO</div>
+          <div className="secciones">
+            <div className="seccionlink">Novedades</div>
+            <div className="seccionlink">Preguntas Frecuentes</div>
+            <div className="seccionlink">Sobre Nosotros</div>
+            <div className="seccionlink">Contactos</div>
+          </div>
+        </nav>
+    )
+  }
+  
+  export default NavBar
+  
