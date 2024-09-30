ced66a54ddba686987dd79b9d2351027d6039496
Matias Salinas <salinasmatiasit@gmail.com>
2024-09-30 00:06:00 -0300

    Carousel added

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
+import { CarouselImages } from './components/CarouselImages'
 import NavBar from './components/NavBar'
 
 function App() {
 
   return (
-    <>
-      <NavBar></NavBar>
-    </>
+      <div className="container">
+        <NavBar></NavBar>
+        <CarouselImages></CarouselImages>
+      </div>
+    
   )
 }
 
---
+++ web/src/components/CarouselImages.jsx
@@ -0,0 +1,33 @@
+import React, { useCallback } from 'react'
+import useEmblaCarousel from 'embla-carousel-react'
+
+export const CarouselImages = () => {
+  const [emblaRef, emblaApi] = useEmblaCarousel({loop:true})
+
+  const scrollPrev = useCallback(() => {
+    if (emblaApi) emblaApi.scrollPrev()
+  }, [emblaApi])
+
+  const scrollNext = useCallback(() => {
+    if (emblaApi) emblaApi.scrollNext()
+  }, [emblaApi])
+
+  return (
+    <div className="carousel">
+      <div className="carousel__viewport" ref={emblaRef}>
+        <div className="carousel__container">
+        <div className="carousel__slide"><img src="https://img.freepik.com/premium-photo/computer-room_38810-865.jpg?w=1380"  /></div>
+        <div className="carousel__slide"><img src="https://live.staticflickr.com/7285/16415815706_ce95427779_k.jpg"  /></div>
+        <div className="carousel__slide"><img src="https://fcefyn.unc.edu.ar/media/images/WhatsApp_Image_2020-10-23_at.2e16d0ba.fill-1100x390-c100.jpg"  /></div>
+        </div>
+      </div>
+      <button className="carousel__prev" onClick={scrollPrev}>
+        
+        &lt;
+      </button>
+      <button className="carousel__next" onClick={scrollNext}>
+        &gt;
+      </button>
+    </div>
+  )
+}
