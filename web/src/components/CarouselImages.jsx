db6275a25b0b1512421a4bd4a0bfc1c0522402e3
Matias Salinas <salinasmatiasit@gmail.com>
2024-10-01 01:25:57 -0300

    comments, .gitignore file moved to root folder, .gitkeep, document format

--- web/src/components/CarouselImages.jsx
+++ web/src/components/CarouselImages.jsx
@@ -1,12 +1,12 @@
 import React, { useCallback } from 'react'
 import useEmblaCarousel from 'embla-carousel-react'
 
-// TODO mapear las imagenes desde un arreglo
+// TODO: Map the images from an array
 function CarouselImages() {
-  
-  const [emblaRef, emblaApi] = useEmblaCarousel({loop:true})
 
-  //funciones para pasar de imagenes
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true })
+
+  // Functions to switch between images
   const scrollPrev = useCallback(() => {
     if (emblaApi) emblaApi.scrollPrev()
   }, [emblaApi])
@@ -19,13 +19,13 @@ function CarouselImages() {
     <div className="carousel">
       <div className="carousel__viewport" ref={emblaRef}>
         <div className="carousel__container">
-        <div className="carousel__slide"><img src="https://img.freepik.com/premium-photo/computer-room_38810-865.jpg?w=1380"  /></div>
-        <div className="carousel__slide"><img src="https://live.staticflickr.com/7285/16415815706_ce95427779_k.jpg"  /></div>
-        <div className="carousel__slide"><img src="https://fcefyn.unc.edu.ar/media/images/WhatsApp_Image_2020-10-23_at.2e16d0ba.fill-1100x390-c100.jpg"  /></div>
+          <div className="carousel__slide"><img src="https://img.freepik.com/premium-photo/computer-room_38810-865.jpg?w=1380" /></div>
+          <div className="carousel__slide"><img src="https://live.staticflickr.com/7285/16415815706_ce95427779_k.jpg" /></div>
+          <div className="carousel__slide"><img src="https://fcefyn.unc.edu.ar/media/images/WhatsApp_Image_2020-10-23_at.2e16d0ba.fill-1100x390-c100.jpg" /></div>
         </div>
       </div>
       <button className="carousel__prev" onClick={scrollPrev}>
-        
+
         &lt;
       </button>
       <button className="carousel__next" onClick={scrollNext}>
--- web/src/components/NavBar.jsx
+++ web/src/components/NavBar.jsx
@@ -1,16 +1,15 @@
 function NavBar() {
-    return (
-        <nav> 
-          <div className="logo">LOGO</div>
-          <div className="secciones">
-            <div className="seccionlink">Novedades</div>
-            <div className="seccionlink">Preguntas Frecuentes</div>
-            <div className="seccionlink">Sobre Nosotros</div>
-            <div className="seccionlink">Contactos</div>
-          </div>
-        </nav>
-    )
-  }
-  
-  export default NavBar
-  
+  return (
+    <nav>
+      <div className="logo">LOGO</div>
+      <div className="secciones">
+        <div className="seccionlink">Novedades</div>
+        <div className="seccionlink">Preguntas Frecuentes</div>
+        <div className="seccionlink">Sobre Nosotros</div>
+        <div className="seccionlink">Contactos</div>
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar
